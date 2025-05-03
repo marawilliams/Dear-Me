1344a9ee0dc8f1c65f20c47f58210b3b33b59375
marawilliams <mara.williams005@gmail.com>
2025-05-03 16:43:53 -0400

    init set up complete, can barebones fxml with no controller

--- src/com/dearme/app/Main.java
+++ src/com/dearme/app/Main.java
@@ -1,4 +1,21 @@
 package com.dearme.app;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/dearme/view/log-in.fxml"));
+        Scene scene = new Scene(loader.load());
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Dear Me - Login");
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args); // This launches the JavaFX application
+    }
 }
